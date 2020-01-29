0082241d0fc75b87060c8e5e580ec7ea0f6086e2
Brianna <bs3864@nyu.edu>
2020-01-29 13:03:07 -0500

    Starting PlayerInput
    Trying to create a generic player input script that you will assign values for what keys are being pressed in the inspector and have it use Ridigbody2D for movement to be able to utilize this script also in my prototyping kit.

---
+++ Week1/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class PlayerInput : MonoBehaviour
+{
+    public KeyCode playerUp, playerDown, playerRight, playerLeft;
+
+    private Rigidbody2D rb2d; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        movePlayer();
+    }
+
+    public void movePlayer()
+    {
+        if(Input.GetKey(playerUp)) {Debug.Log("player moved up");}
+        if(Input.GetKey(playerDown)){}
+        if(Input.GetKey(playerLeft)){}
+        if(Input.GetKey(playerRight)){}
+        
+    }
+}
