3a3ed27354f68cbd12c458ad77f2fc8a4b18b7d7
Brianna <bs3864@nyu.edu>
2020-02-23 01:42:55 -0500

    Statemachine Tutorial for AI

--- Week1/Assets/Scripts/Tree/PlayerController.cs
+++ Week1/Assets/Scripts/Tree/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 6.0f;
     public float gravity = 15.0f;
-    public float jumpSpeed = 10f;
+    public float jumpStrength = 10f;
 
     private CharacterController controller;  
     // https://docs.unity3d.com/ScriptReference/CharacterController.html 
@@ -16,12 +16,29 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (controller.isGrounded)
+        {
+            movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            movement = transform.TransformDirection(movement);
+            movement *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                movement.y = jumpStrength; 
+            }
+        }
+        else
+        {
+            movement.y -= gravity * Time.deltaTime; 
+        }
+
+        controller.Move(movement * Time.deltaTime);
     }
+    
 }
