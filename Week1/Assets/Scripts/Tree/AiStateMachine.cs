b2680e758f0a152317b7fa1891739c58ab0f235a
Brianna <bs3864@nyu.edu>
2020-02-23 10:26:09 -0500

    Statemachine!

--- Week1/Assets/Scripts/Tree/AiStateMachine.cs
+++ Week1/Assets/Scripts/Tree/AiStateMachine.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class AiStateMachine : MonoBehaviour
 {
+    //headers create sections in inspector 
+    //also it yells at you until you put the variable
+    [Header("From AIStateMachine")] 
+    public GameObject targetPlayer;
+
+    public GameObject[] wayPointList;
+
+    protected int currentWayPoint;
+    protected Vector3 nextWayPointPosition;
+
+    protected CharacterController controller; // if this is the same we can make a reference in serviced
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentWayPoint = 0;
+        AiInitialization();
     }
 
+    protected virtual void AiInitialization()
+    {
+        
+    }
     // Update is called once per frame
     void Update()
+    {
+        AiTick();
+    }
+
+    protected virtual void AiTick()
     {
         
     }
